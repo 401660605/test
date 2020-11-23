c874e774a8213ce106a776d474344276fe5d6683
401660605 <59038575+401660605@users.noreply.github.com>
2020-11-23 14:29:58 +0800

    Update main.py
    111

--- main.py
+++ main.py
@@ -15,5 +15,7 @@ if __name__ == '__main__':
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+   print_hi('PyCharm')
+
 
 
